181862e7df9f477034d606b0881c55c33bad5d71
Alistair Madden <Alistair.John.Madden@gmail.com>
2017-01-27 01:27:34 +0000

    WAT?!

--- blockAlternativeMaxOptimisations.cpp
+++ blockAlternativeMaxOptimisations.cpp
@@ -693,7 +693,6 @@ int computeP() {
 
 
     // Kind of manual synchronisation
-    #pragma omp parallel for
     for (int i = 0; i < indicesInDomainNonBoundarySize; i++) {
       std::cout << "hi" << std::endl;
       //p[index] += -omega * residuals[index] / 6.0 * getH() * getH();
