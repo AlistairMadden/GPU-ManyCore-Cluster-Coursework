a677f3da309f63281ca9ca5b55b87e71707488ce
Alistair Madden <Alistair.John.Madden@gmail.com>
2017-01-27 12:42:14 +0000

    Does this one do any better?

--- karman_vec_computeP.cpp
+++ karman_vec_computeP.cpp
@@ -90,6 +90,7 @@ on YouTube, e.g.
 #include <sstream>
 #include <fstream>
 #include <cmath>
+#include <algorithm>
 
 
 /**
@@ -658,7 +659,7 @@ int computeP() {
     double* residuals = new double[(numberOfCellsPerAxisZ+2)*(numberOfCellsPerAxisY+2)*(numberOfCellsPerAxisX+2)];
     for (int iz=1; iz<numberOfCellsPerAxisZ+1; iz++) {
       for (int iy=1; iy<numberOfCellsPerAxisY+1; iy++) {
-        #pragma simd
+        #pragma omp parallel for
         for (int ix=1; ix<numberOfCellsPerAxisX+1; ix++) {
           if ( cellIsInside[getCellIndex(ix,iy,iz)] ) {
             double residual = rhs[ getCellIndex(ix,iy,iz) ] +
@@ -672,6 +673,7 @@ int computeP() {
                 - 1.0 * p[ getCellIndex(ix,iy,iz+1) ]
                 + 6.0 * p[ getCellIndex(ix,iy,iz) ]
               );
+#pragma omp atomic update
             globalResidual += residual * residual;
             residuals[getCellIndex(ix,iy,iz)] = residual;
           }
@@ -680,9 +682,9 @@ int computeP() {
     }
 
     // Kind of manual synchronisation
+#pragma omp parallel for
     for (int iz=1; iz<numberOfCellsPerAxisZ+1; iz++) {
       for (int iy = 1; iy < numberOfCellsPerAxisY + 1; iy++) {
-        #pragma simd
         for (int ix = 1; ix < numberOfCellsPerAxisX + 1; ix++) {
           if (cellIsInside[getCellIndex(ix, iy, iz)]) {
             p[getCellIndex(ix,iy,iz)] += -omega * residuals[getCellIndex(ix,iy,iz)] / 6.0 * getH() * getH();
